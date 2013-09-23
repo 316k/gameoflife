dd65c73a1dd4d85da59bdebe7ed9367f833a6fb2
Nicolas Hurtubise <316k@legtux.org>
2013-09-23 12:58:43 -0400

    Renames the project and adds a draft for the user interface

---
+++ src/ca/qc/bdeb/inf203/jaudiblegameoflife/JAudibleGameOfLife.java
@@ -0,0 +1,11 @@
+package ca.qc.bdeb.inf203.jaudiblegameoflife;
+
+/**
+ * @author Nicolas Hurtubise
+ */
+public class JAudibleGameOfLife {
+
+    public static void main(String[] args) {
+        MainWindow pMainWindow = new MainWindow();
+    }
+}
