b182e6df1b4066514062468adb9a3b8ecd067247
Nicolas Hurtubise <316k@legtux.org>
2013-10-17 20:22:41 -0400

    Adds an @interface : @sketch. (I hope I won't have to use it !

---
+++ src/java/lang/Sketch.java
@@ -0,0 +1,10 @@
+package java.lang;
+
+import java.lang.annotation.*;
+import static java.lang.annotation.ElementType.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value={CONSTRUCTOR, FIELD, LOCAL_VARIABLE, METHOD, PACKAGE, PARAMETER, TYPE})
+public @interface Sketch {
+}
