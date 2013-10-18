869eef81bda334a6d5bf8b28869e46000d9e106b
Nicolas Hurtubise <316k@legtux.org>
2013-10-17 20:22:41 -0400

    Adds an @interface : @sketch. (I hope I won't have to use it !
    Conflicts:
    	src/java/lang/Sketch.java

--- src/java/lang/Sketch.java
+++ src/java/lang/Sketch.java
@@ -3,12 +3,15 @@ package java.lang;
 import java.lang.annotation.*;
 import static java.lang.annotation.ElementType.*;
 
+<<<<<<< HEAD
 /**
  * Définit si un bout de code a été écrit de façon sketch. E.g.: Transformer
  * un nombre en String et vérifier si le charAt(nombre.length-1) = 0, 2, 4, 6, ou 8
  * pour tester s'il est pair.
  * @author k
  */
+=======
+>>>>>>> b182e6d... Adds an @interface : @sketch. (I hope I
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(value={CONSTRUCTOR, FIELD, LOCAL_VARIABLE, METHOD, PACKAGE, PARAMETER, TYPE})
